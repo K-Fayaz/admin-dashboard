8ebd115ded6b799b4c3436e68e39da11e13044be
Fayaz <kfayaz1407@gmail.com>
2025-12-30 14:34:02 +0530

    feat: project setup and login ui

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+export default function AdminPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="w-full max-w-4xl px-6">
+        <div className="rounded-lg bg-white p-8 shadow-lg dark:bg-zinc-900">
+          <h1 className="text-center text-3xl font-semibold text-black dark:text-zinc-50">
+            This is admin page
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
